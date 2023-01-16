5655787363974de09757eb3809ddd69d8fb62467
David Kipnis <pankimatick@gmail.com>
2023-01-16 15:10:56 -0800

    Added deck generation for setup; created shuffle method for deck (built-in Arcade method doesn't work)

--- Card.py
+++ Card.py
@@ -12,3 +12,9 @@ class Card(arcade.Sprite):
 
     def __call__(self, image_path, scaling, suit, value):
         return Card(image_path=image_path, scaling=scaling, suit=suit, value=value)
+
+    def get_suit(self):
+        return self.suit
+
+    def get_value(self):
+        return self.value
--- main.py
+++ main.py
@@ -4,6 +4,17 @@ import ButtonStyles as buttonStyles
 import GameData as gameData
 import Card
 from screeninfo import get_monitors
+import os
+import random
+
+
+class ShuffleButton(arcade.gui.UIFlatButton):
+    def __init__(self, deck: arcade.SpriteList, **kwargs):
+        super().__init__(**kwargs)
+        self.deck = deck
+
+    def on_click(self, event: arcade.gui.UIOnClickEvent):
+        shuffle_deck(self.deck)
 
 
 class QuitButton(arcade.gui.UIFlatButton):
@@ -44,11 +55,13 @@ class SetupMenu(arcade.Window):
         self.v_box = arcade.gui.UIBoxLayout()
         self.h_box = arcade.gui.UIBoxLayout(vertical=False)
 
-        selection_label = arcade.gui.UIFlatButton(text="Select # of Players", style=buttonStyles.disabled_button, width=300, height=100)
+        selection_label = arcade.gui.UIFlatButton(text="Select # of Players", style=buttonStyles.disabled_button,
+                                                  width=300, height=100)
         self.v_box.add(selection_label.with_space_around(bottom=20))
 
         for i in range(1, 5):
-            selection_button = PlayerSelectionButton(text=str(i), width=100, height=100, num=i, style=buttonStyles.red_style)
+            selection_button = PlayerSelectionButton(text=str(i), width=100, height=100, num=i,
+                                                     style=buttonStyles.red_style)
             self.h_box.add(selection_button.with_space_around(right=20, left=20))
 
         self.v_box.add(self.h_box.with_space_around(bottom=100))
@@ -73,6 +86,9 @@ class GameWindow(arcade.Window):
     def __init__(self, screen_width, screen_height, player_count, title):
         super().__init__(screen_width, screen_height, title)
 
+        self.manager = arcade.gui.UIManager()
+        self.manager.enable()
+
         self.player_count = player_count
 
         self.card_list = None
@@ -80,22 +96,72 @@ class GameWindow(arcade.Window):
         arcade.set_background_color(arcade.color.GREEN_YELLOW)
 
     def setup(self):
-        self.card_list = arcade.SpriteList()
+        self.card_list = generate_card_deck()
+        shuffle_button = ShuffleButton(text="Shuffle", width=150, height=100, deck=self.card_list)
 
-        test_card = Card.Card(image_path="Card_Sprites/Hearts_King.png",
-                              scaling=gameData.CARD_SPRITE_SCALING,
-                              suit="Hearts", value=13)
-
-        test_card.center_y = gameData.SCREEN_Y / 2
-        test_card.center_x = gameData.SCREEN_X / 2
-
-        self.card_list.append(test_card)
+        self.manager.add(
+            arcade.gui.UIAnchorWidget(
+                align_x=500,
+                align_y=-300,
+                child=shuffle_button)
+        )
 
     def on_draw(self):
         self.clear()
         arcade.start_render()
 
+        self.manager.draw()
         self.card_list.draw()
+
+
+def generate_card_deck():
+    deck = arcade.SpriteList()
+
+    counter = 0
+
+    for filename in os.listdir('Card_Sprites'):
+        if filename == 'Back.png':
+            continue
+        file_path = 'Card_Sprites/' + filename
+
+        filename = filename.split('_')
+        card_suit = filename[0]
+        card_val = filename[1].split('.')[0]
+
+        cur_card = Card.Card(image_path=file_path,
+                             scaling=gameData.CARD_SPRITE_SCALING,
+                             suit=card_suit, value=card_val)
+
+        cur_card.center_y = 200 + (200 * (counter // 22))
+        cur_card.center_x = 85 + (40 * (counter % 22))
+        counter += 1
+
+        deck.append(cur_card)
+
+    return deck
+
+
+def shuffle_deck(deck):
+    for i in range(len(deck) - 1, 1, -1):
+        new_index = random.randint(0, i - 1)
+
+        card_1 = deck[i]
+        card_2 = deck[new_index]
+
+        deck.remove(card_1)
+        deck.remove(card_2)
+
+        temp_x = card_1.center_x
+        temp_y = card_1.center_y
+
+        card_1.center_x = card_2.center_x
+        card_1.center_y = card_2.center_y
+
+        card_2.center_x = temp_x
+        card_2.center_y = temp_y
+
+        deck.insert(new_index, card_1)
+        deck.insert(i, card_2)
 
 
 def get_screen_dimensions():
@@ -108,9 +174,10 @@ def get_screen_dimensions():
             screen_height = m.height
             return screen_width, screen_height
 
+    return None
+
 
 def main():
-
     screen_dimensions = get_screen_dimensions()
 
     if screen_dimensions is None:
